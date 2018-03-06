1b43ca65438b7fdc392e6c74a14f0b7c1b1fec13
Hugo Marquez <hoberstudent@gmail.com>
2018-03-06 11:23:19 -0600

    Create code1.py

---
+++ code1.py
@@ -0,0 +1,26 @@
+#!usr/bin/env python3
+from ev3dev.ev3 import*
+
+btn = Button()
+
+cl = ColorSensor()
+
+cl.mode = 'COL-REFLECT'
+
+mB = LargeMotor('outB')
+mC = LargeMotor('outC')
+
+mB.run_forever(speed_sp=100)
+mC.run_forever(speed_sp=100)
+
+while not btn.any():
+  if cl.value()<30:
+    mB.stop(stop_action='break')
+    mC.stop(stop_action='break')
+    
+  else:
+    mB.stop(stop_action='break')
+    mC.stop(stop_action='break')
+    
+# mB.stop(stop_action='break')
+# mC.stop(stop_action='break')
