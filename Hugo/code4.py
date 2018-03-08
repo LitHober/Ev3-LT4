cf04fc7d2f860595476594998babe749b0c41424
Hugo Marquez <hoberstudent@gmail.com>
2018-03-08 15:43:05 -0600

    Create code4.py

---
+++ Hugo/code4.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from ev3dev.ev3 import *
+
+# Sensor de color
+cS = ColorSensor()
+cS.mode = 'COL-REFLECT'
+
+# Sensor de proximidad
+iS = InfraredSensor() 
+iS.mode = 'IR-PROX'
+
+mA = largeMotor('outB')
+mB = largeMotor('outC')
+
+mA.run_forever(speed_sp=100)
+mB.run_forever(speed_sp=100)
+
+if iS.value()<20:
+    mA.run_forever(speed_sp=-100)
+    mB.run_forever(speed_sp=-100)
+    
+
+
+#negro 4
+#azul 11
+#cafe 30
+#rojo 57
