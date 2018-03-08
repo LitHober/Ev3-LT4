82f56e0ed11272d3262f9163b3a6ea2e18f67fb7
Hugo Marquez <hoberstudent@gmail.com>
2018-03-08 14:57:51 -0600

    Update code3.py

--- code3.py
+++ code3.py
@@ -2,27 +2,30 @@
 
 from ev3dev.ev3 import *
 
-btn = Button()
-
+# Sensor de color
 cS = ColorSensor()
-
 cS.mode = 'COL-REFLECT'
 
+# Sensor de proximidad
+iS = InfraredSensor() 
+iS.mode = 'IR-PROX'
 
-motorA = LargeMotor('outB')
-motorB = LargeMotor('outC')
+mA = largeMotor('outB')
+mB = largeMotor('outC')
 
-motorA.inversed(speed_sp=200)
-motorB.inversed(speed_sp=200)
+motorA.run_forever(speed_sp=100)
+motorB.run_forever(speed_sp=100)
 
-
-
-
+if iS.value()<20:
+    mA.inversed(speed_sp=300)
+    mB.inversed(speed_sp=300)
 
 
 #negro 4
 #azul 11
 #cafe 30
 #rojo 57
+
+
 
  
