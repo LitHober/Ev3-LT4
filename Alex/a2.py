d16fc7158e1a4b9edfb85b83f9615b47f5c61436
Alejandro Díaz Ávalos <diaz.ctrl01@gmail.com>
2018-03-09 08:42:40 -0600

    Add files via upload

---
+++ Alex/a2.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+from ev3dev.ev3 import *
+from time import sleep
+
+#sensors
+infra_s = InfraredSensor()
+assert infra_s.connected, "Conecte un solo sensor de infrarrojos a cualquier puerto de sensor"
+
+color_s = ColorSensor()
+assert color_s.connected, "Conecte un sensor de color a cualquier puerto"
+
+#modes
+infra_s.mode = 'IR-PROX'
+color_s = 'COL-REFLECT'
+
+#vars
+button = Button()
+reflection = color_s.value()
+distance = infra_s.value()
+
+while button.any():
+    print(reflection)
+    sleep(0.5)
+
+'''
+if distance < 60:
+    Leds.set_color(Leds.LEFT, Leds.RED)
+else:
+    Leds.set_color(Leds.LEFT, Leds.GREEN)
+'''
+
+#enciende el led izquierdo en verde antes de acabar
+Sound.beep()
+Leds.set_color(Leds.LEFT, Leds.GREEN)
