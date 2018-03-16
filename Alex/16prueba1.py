e33e1fd341a9b4435dea488dc284ea88bd69d846
Alejandro Díaz Ávalos <diaz.ctrl01@gmail.com>
2018-03-16 08:27:51 -0600

    Create 16prueba1.py

---
+++ Alex/16prueba1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import threading
+import time
+
+#sensors & motors
+infra_s = InfraredSensor()
+color_s = ColorSensor()
+
+#motor_B = LargeMotor('outB')
+#motor_C = LargeMotor('outC')
+
+#modes
+infra_s.mode = 'IR-PROX'
+color_s.mode = 'COL-COLOR'
+
+#vars
+distance = infra_s.value()
+
+#funciones
+def funcion1():
+    mB = LargeMotor('outB')
+    mC = LargeMotor('outC')
+
+    mB.run_forever(speed = 900)
+    time.sleep(5)
+    mC.stop(stop_action = "hold")
+    time.sleep(5)
+
+hilo = threading.Thread( name = 'funcion1', target = funcion1)
+
+hilo.start()
