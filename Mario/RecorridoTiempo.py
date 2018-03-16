d64bb92b9d3401598807696f0e9c308334b42c3d
MarioMtzc <macema30@gmail.com>
2018-03-16 11:41:13 -0600

    Update RecorridoTiempo.py

--- Mario/RecorridoTiempo.py
+++ Mario/RecorridoTiempo.py
@@ -23,7 +23,7 @@ while not btn.any():    #Se sale del loop cuando presionas cualquier boton
 		else:
 			direccion = 0
 	elif direccion == 1: 
-      	mB.run_timed(time_sp=3000, speed_sp=500)
+      		mB.run_timed(time_sp=3000, speed_sp=500)
 		mC.run_timed(time_sp=3000, speed_sp=500)
 		mB.run_to_rel_pos(position_sp=500, speed_sp=200, stop_action="brake")  
 		mC.run_to_rel_pos(position_sp=-500, speed_sp=200, stop_action="brake")
