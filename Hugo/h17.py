04e395ba444d431d2783aa9175fa0989b4d3fae5
Hugo Marquez <hoberstudent@gmail.com>
2018-03-26 13:44:11 -0600

    Add files via upload

---
+++ Hugo/h17.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from ev3dev.ev3 import *
+from time import sleep
+
+#Sensores y motores
+cl = ColorSensor('in3')
+ir = InfraredSensor('in2') 
+motorIzq = LargeMotor('outB')
+motorDer = LargeMotor('outC')
+
+#Modos
+cl.mode = 'COL-COLOR'
+ir.mode = 'IR-PROX'
+
+#Variables
+btn = Button()
+distance = ir.value() 
+
+while not btn.any():
+    if ir.value() < 20:
+        motorA.run_to_rel_pos(position_sp=-500, speed_sp=200, stop_action="brake")  
+        motorB.run_to_rel_pos(position_sp=-500, speed_sp=200, stop_action="brake") 
+
+
+motorIzq.stop(stop_action='brake')
+motorDer.stop(stop_action='brake')
